30d4e9247c9b4a673ac1aea2837bb69af4ea9384
Marcus Vinicius Ruas de Andrade <marcusruas.andrade@gmail.com>
2025-08-21 23:02:26 -0300

    Updated model

--- Alduin.Core/Models/AlduinSettings.cs
+++ Alduin.Core/Models/AlduinSettings.cs
@@ -19,7 +19,7 @@ namespace Alduin
         /// <summary>
         /// The model that open AI will use for its web socket. the default is gpt-4o-realtime-preview-2024-12-17.
         /// </summary>
-        public string RealtimeModel { get; set; } = "gpt-4o-realtime-preview-2024-12-17";
+        public string RealtimeModel { get; set; } = "gpt-4o-realtime-preview-2025-06-03";
         /// <summary>
         /// The type of voice the AI will have. The supported voices are located in the OpenAI Documentation <see cref="https://platform.openai.com/docs/api-reference/audio/createSpeech"/>
         /// </summary>
@@ -38,6 +38,7 @@ namespace Alduin
         public int ClientInactivityTimeout { get; set; } = 600;
 
         internal const string OPEN_AI_WEBSOCKET_URL = "wss://api.openai.com/v1/realtime?model={0}";
+
         internal void EnsureIsValid()
         {
             if (string.IsNullOrWhiteSpace(OpenAIApiKey))
